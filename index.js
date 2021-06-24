61baa9d8245bdeb81c469cf9e7f9de2abe68a940
woodendoors7 <60623935+woodendoors7@users.noreply.github.com>
2021-06-24 19:40:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const fs = require('fs');
 var dltmsg;
-var version = "0.1.2";
+var version = "0.1.1";
 var thefilenamething = "./Settings and database/deletedmsg.json"
 var data;
 var options;
